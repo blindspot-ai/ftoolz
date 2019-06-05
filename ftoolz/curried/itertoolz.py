de16a3b467d1d3afe2780ee77915beb1d5163629
Martin Matyasek <martin.matyasek@blindspot.ai>
2019-06-05 22:46:49 +0200

    Clean up curried.itertoolz and add typing

--- ftoolz/curried/itertoolz.py
+++ ftoolz/curried/itertoolz.py
@@ -1,12 +1,31 @@
+from typing import Callable, Iterable, Optional, Tuple
+
 from cytoolz import curry
 
 import ftoolz.itertoolz as it
+from ftoolz.typing import Map, Seq
 
-associate = curry(it.associate)
-associate_to = curry(it.associate_to)
-collect = curry(it.collect)
-empty = curry(it.empty)
-enumerate_with_final = curry(it.enumerate_with_final)
-filter_not_none = curry(it.filter_not_none)
-find = curry(it.find)
-first = curry(it.first)
+# TODO: Add typing for all levels of curry
+
+PredE = Callable[[it.E], bool]
+ItE = Iterable[it.E]
+SeqE = Seq[it.E]
+
+associate: Callable[
+    [Callable[[it.B], it.A]],
+    Callable[[Iterable[it.B]], Map[it.A, it.B]]
+] = curry(it.associate)
+
+associate_to: Callable[
+    [Callable[[it.B], it.A]],
+    Callable[[Callable[[it.B], it.C], Iterable[it.B]], Map[it.A, it.C]]
+] = curry(it.associate_to)
+
+find: Callable[[PredE], Callable[[ItE], Optional[it.E]]] = \
+    curry(it.find)
+
+split_by: Callable[[PredE], Callable[[ItE], Tuple[ItE, ItE]]] = \
+    curry(it.split_by)
+
+take: Callable[[int], Callable[[ItE], SeqE]] = \
+    curry(it.take)
