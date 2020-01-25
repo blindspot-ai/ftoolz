c49d776a87e0be18de230b34f6f23195af4fdccc
Martin Matyasek <martin.matyasek@blindspot.ai>
2020-01-25 13:45:13 +0100

    Use seq instead of tuple in Ring

--- ftoolz/adt/ring.py
+++ ftoolz/adt/ring.py
@@ -3,7 +3,7 @@ from typing import Iterable, Iterator, Sized, TypeVar
 
 from cytoolz.functoolz import do
 
-from ftoolz.typing import Seq
+from ftoolz.typing import Seq, seq
 
 _E = TypeVar('_E')
 
@@ -12,7 +12,7 @@ class Ring(Iterator[_E], Sized):  # pylint: disable=E0239
     """Fixed-size in-memory rotating list of elements"""
 
     # pylint: disable=W0231
-    def __init__(self, elements: Iterable[_E] = tuple()) -> None:
+    def __init__(self, elements: Iterable[_E] = seq()) -> None:
         self._ring = deque(elements)
 
     def __len__(self) -> int:
@@ -27,4 +27,4 @@ class Ring(Iterator[_E], Sized):  # pylint: disable=E0239
         return do(self._ring.append, self._ring.popleft())
 
     def state(self) -> Seq[_E]:
-        return tuple(self._ring)
+        return seq(self._ring)
